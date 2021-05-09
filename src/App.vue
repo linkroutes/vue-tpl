1781fc7bc52bf086f8002ac93c468052fb00b9fd
陈新垲 <chenxinkai@pconline.com.cn>
2021-05-09 17:40:54 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,4 @@
 
 <style lang="scss">
   @import '@/assets/scss/app.scss';
-  #app {
-    font-family: -apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Segoe UI,Arial,Roboto,PingFang SC,miui,Hiragino Sans GB,Microsoft Yahei,sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
 </style>
